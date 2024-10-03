811d2b9d6e2b1177b8d288e2a4defd95558f1fe6
xocoee <141821998+xocoee@users.noreply.github.com>
2024-10-03 18:38:49 +0300

    implement Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./App.scss";
+
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="app-container">
+      <Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="header-logo">
+            xocoee
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
