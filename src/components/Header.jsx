e6f5991db723913fdf1b3b853bcddb496600630d
xocoee <141821998+xocoee@users.noreply.github.com>
2024-10-04 22:58:50 +0300

    add Social Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,61 @@
-import './Header.scss'
+import { useState } from "react";
+import "./Header.scss";
+
+import linkedinLogo from "../assets/socialLink/linkedin.svg";
+import githubLogo from "../assets/socialLink/github.svg";
+import telegramLogo from "../assets/socialLink/telegram.svg";
 
 const Header = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowLinks(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowLinks(false);
+  };
+
   return (
     <>
       <div className="header">
-        <div className="header-logo">
+        <div className="header-content">
+          <div
+            className="header-logo"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
             xocoee
+            <div className={`social-links ${showLinks ? "visible" : ""}`}>
+              <a
+                href="https://www.linkedin.com/in/xocoee/"
+                className="social-link-logo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedinLogo} alt="Linkedin logo" />
+              </a>
+
+              <a
+                href="https://github.com/xocoee"
+                className="social-link-logo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={githubLogo} alt="Github logo" />
+              </a>
+
+              <a
+                href="https://t.me/xocoee"
+                className="social-link-logo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={telegramLogo} alt="Telegram logo" />
+              </a>
+            </div>
+          </div>
+          <div className="header-logo">xocoee</div>
         </div>
       </div>
     </>
