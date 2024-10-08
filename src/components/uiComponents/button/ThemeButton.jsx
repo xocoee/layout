9c9041960e5bf8a9dd88c04a10160df9fededfec
xocoee <141821998+xocoee@users.noreply.github.com>
2024-10-08 22:55:17 +0300

    change Theme Button

--- src/components/uiComponents/button/ThemeButton.jsx
+++ src/components/uiComponents/button/ThemeButton.jsx
@@ -2,23 +2,28 @@ import { useState } from "react";
 import "./ThemeButton.scss";
 
 const ThemeButton = () => {
-  const [hovered, setHovered] = useState(false);
-  const [theme, setTheme] = useState("light")
+  const [showToggle, setShowToggle] = useState(false);
 
+  const handleMouseEnter = () => {
+    setShowLinks(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowLinks(false);
+  };
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
     <>
-      <button
-        className="theme-button"
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        onClick={toggleTheme}
-      >
-        <span>{hovered ? (theme === "light" ? "dark" : "light") : theme}</span>
-      </button>
+      <div className="theme-button">
+        Theme
+      </div>
+      <div className={`switch-theme ${showLinks ? "visible" : ""}`}>
+         <div>Dark</div>
+         <div>Light</div>
+      </div>
     </>
   );
 };
